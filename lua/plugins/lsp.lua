ffd381e9c7b1a144e8765e3c97de1c94ea7ef08b
Avishay Matayev <avishay@blocka.com>
2023-01-07 16:51:33 +0200

    nvim-lspconfig: Configure completeFunctionCalls for tsserver

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,36 @@
 return {
   {
     "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+    },
     config = function()
-      require'lspconfig'.tsserver.setup {}
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      local on_attach = function(client, bufnr)
+        -- If the LSP supports formatting, allow for format-on-save through LSP
+        if client.server_capabilities.document_formatting then
+          vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+        end
+      end
+
+      require'lspconfig'.tsserver.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          completions = {
+            completeFunctionCalls = true
+          }
+        },
+        init_options = {
+          hostInfo = "neovim",
+          preferences = {
+            includeCompletionsWithSnippetText = true,
+            includeCompletionsForImportStatements = true,
+          },
+        },
+      }
     end
   },
   {
