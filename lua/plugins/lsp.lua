af96119bfbbb176d3481e48dc0026d2935834678
Avishay Matayev <avishay@blocka.com>
2023-01-07 15:45:32 +0200

    Plugins: Add mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,25 @@
 return {
   {
     "neovim/nvim-lspconfig"
-  }
+  },
+  {
+    "williamboman/mason.nvim",
+    config = true,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim"
+    },
+    config = function()
+      require("mason-lspconfig").setup {
+        ensure_installed = {
+          "tsserver",
+          "pyright",
+        },
+      }
+    end
+  },
 }
