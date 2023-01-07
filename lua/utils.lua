40f52fd94ed8246f7f1c17e0784757ce29596d27
Avishay Matayev <avishay@blocka.com>
2023-01-08 00:30:45 +0200

    utils: Add missing qf related functions

--- lua/utils.lua
+++ lua/utils.lua
@@ -70,6 +70,58 @@ M.reload_config = function()
   end, vim.fn.split(vim.fn.execute("scriptnames"), "\n"))
 end
 
+-- 'q': find the quickfix window
+-- 'l': find all loclist windows
+function M.find_qf(type)
+  local wininfo = vim.fn.getwininfo()
+  local win_tbl = {}
+  for _, win in pairs(wininfo) do
+    local found = false
+    if type == "l" and win["loclist"] == 1 then
+      found = true
+    end
+    -- loclist window has 'quickfix' set, eliminate those
+    if type == "q" and win["quickfix"] == 1 and win["loclist"] == 0 then
+      found = true
+    end
+    if found then
+      table.insert(win_tbl, { winid = win["winid"], bufnr = win["bufnr"] })
+    end
+  end
+  return win_tbl
+end
+
+-- open quickfix if not empty
+function M.open_qf()
+  local qf_name = "quickfix"
+  local qf_empty = function() return vim.tbl_isempty(vim.fn.getqflist()) end
+  if not qf_empty() then
+    vim.cmd("copen")
+    vim.cmd("wincmd J")
+  else
+    print(string.format("%s is empty.", qf_name))
+  end
+end
+
+-- enum all non-qf windows and open
+-- loclist on all windows where not empty
+function M.open_loclist_all()
+  local wininfo = vim.fn.getwininfo()
+  local qf_name = "loclist"
+  local qf_empty = function(winnr) return vim.tbl_isempty(vim.fn.getloclist(winnr)) end
+  for _, win in pairs(wininfo) do
+    if win["quickfix"] == 0 then
+      if not qf_empty(win["winnr"]) then
+        -- switch active window before ':lopen'
+        vim.api.nvim_set_current_win(win["winid"])
+        vim.cmd("lopen")
+      else
+        print(string.format("%s is empty.", qf_name))
+      end
+    end
+  end
+end
+
 -- toggle quickfix/loclist on/off
 -- type='*': qf toggle and send to bottom
 -- type='l': loclist toggle (all windows)
