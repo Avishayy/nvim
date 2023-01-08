ab921bbcb5d71aa3c1cadc551313e6c2c982672f
Avishay Matayev <avishay@blocka.com>
2023-01-08 10:41:51 +0200

    LSP: Setup pyright
    Easy!

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -100,6 +100,11 @@ return {
           },
         },
       }
+
+      require'lspconfig'.pyright.setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }
     end
   },
   {
