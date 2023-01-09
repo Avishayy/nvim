dae6e628616c572f62ae9227a032f36d1ec7fe7b
Avishay Matayev <avishay@blocka.com>
2023-01-09 21:09:43 +0200

    colorscheme: Set gruvbox diff highlights
    NO stupid reverse (which makes the backgroud annoying as hell)
    
    I changed it to have the same colors but with a lighter grey background,
    specifically the colors that were used from the gruvbox palette are:
    background dark1
    diffadd fg bright_green
    diffdelete fg bright_red
    diffchange fg dark4 (the unchanged part of a line)
    difftext fg bright_aqua (text that gets added to a line)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -11,6 +11,11 @@ return {
           NormalFloat = { bg = "#282828" },
           -- Sign column to look like regular background
           SignColumn = { bg = "#1d2021" },
+          -- Diff without reverse
+          DiffAdd = { reverse = false, fg = "#b8bb26", bg = "#3c3836", },
+          DiffDelete = { reverse = false, fg = "#fb4934", bg = "#3c3836", },
+          DiffChange = { reverse = false, fg = "#7c6f64", bg = "#3c3836", },
+          DiffText = { reverse = false, fg = "#8ec07c", bg = "#3c3836", },
         }
       }
       vim.cmd [[ colorscheme gruvbox ]]
