4ddc0b09b12ef7c846717f3d42b1f04c9f99b703
Avishay Matayev <avishay@blocka.com>
2023-01-09 20:22:38 +0200

    lsp: add missing lua config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -85,9 +85,13 @@ return {
             },
             workspace = {
               library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
             },
             completion = {
               callSnippet = "Replace",
+            },
+            telemetry = {
+              enable = false,
             },
           },
         }
