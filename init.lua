9d83748d2c234d64b4737fe1b030ac856e06dd74
Avishay Matayev <avishay@blocka.com>
2023-01-11 13:57:06 +0200

    Plugins: Set up nvim-dap-python
    As for the weird insert to vim._so_trails see the issue in neovim [1]
    
    1. https://github.com/neovim/neovim/issues/21749

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+-- mac doesnt' load dylib libraries by default
+table.insert(vim._so_trails, "/?.dylib")
+
 require("options")
 require("abbrevs")
 require("keymaps")
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,17 +4,40 @@ return {
     dependencies = {
       "mfussenegger/nvim-dap",
       "williamboman/mason.nvim",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
+      "Joakker/lua-json5",
     },
     lazy = false,
     config = function()
+      require("dap-python").setup("python3")
+
       require("mason-nvim-dap").setup({
         ensure_installed = {
           "python",
         }
       })
+
+      -- TODO: add autocmd that loads it every time we modify it?
+      require('dap.ext.vscode').load_launchjs()
     end,
   },
   {
     "mfussenegger/nvim-dap",
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    config = true,
+  },
+  {
+    "Joakker/lua-json5",
+    lazy = false,
+    build = "./install.sh",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require('dap.ext.vscode').json_decode = require'json5'.parse
+    end
   },
 }
