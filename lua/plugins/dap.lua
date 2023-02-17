1a26cd7b128b8f3dbd488c5534f43cedc04cc25d
Avishay Matayev <me@avishay.dev>
2023-02-17 16:39:12 +0200

    lua-json5: Copy dylib to so
    ... and remove dylib loading hack.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -32,7 +32,7 @@ return {
   {
     "Joakker/lua-json5",
     lazy = false,
-    build = "./install.sh",
+    build = "./install.sh && cp lua/json5.{dylib,so}",
     dependencies = {
       "mfussenegger/nvim-dap",
     },
