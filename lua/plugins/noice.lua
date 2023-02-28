7dd16d85ddf8e3b82fb9a3913e0825fc7eb44c75
Avishay Matayev <me@avishay.dev>
2023-02-28 17:11:39 +0200

    noice: Hide written messages

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -24,6 +24,10 @@ return {
             filter = { filter = { event = "msg_show", kind = "search_count" } },
             opts = { skip = true },
           },
+          {
+            filter = { event = "msg_show", kind = "", find = "written" },
+            opts = { skip = true },
+          },
         },
       }
     end,
