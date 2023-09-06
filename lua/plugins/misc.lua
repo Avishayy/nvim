54b8525b78ee64743771b48a391eeb2adf724422
Avishay Matayev <me@avishay.dev>
2023-09-06 19:43:28 +0300

    neorg: Change icon theme to diamond

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -79,7 +79,11 @@ return {
       require("neorg").setup {
         load = {
           ["core.defaults"] = {},  -- Loads default behaviour
-          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.concealer"] = {
+            config = {
+              icon_preset = "diamond",
+            },
+          }, -- Adds pretty icons to your documents
           ["core.dirman"] = {      -- Manages Neorg workspaces
             config = {
               workspaces = {
